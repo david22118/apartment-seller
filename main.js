f1adf9f992eab2c084d554cf135a390b0e088e53
davidy22118 <davidy22118@gmail.com>
2020-04-21 10:14:51 +0300

    apartment-pro

--- main.js
+++ main.js
@@ -6,15 +6,18 @@ $("button").on("click", function () {
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
     let immediate = $("#immed-y")
-
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    let parking = $("#park-y")
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate,parking)
     renderApts(relevantApts)
 })
 
 const renderApts = function (apartments) {
     $("#results").empty()
     console.log(apartments) //array of apartments to render
-    //Your code goes here.
+    const source = $('#store-template').html()
+    const template = Handlebars.compile(source)//Your code goes here.
+    let newHTML = template({ apartments:apartments })
+    $('#results').append(newHTML)
 }
 
 renderApts(apartments) //renders apartments when page loads
