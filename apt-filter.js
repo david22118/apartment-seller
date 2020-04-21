058314544f74238039dab51338d7f43a2674124e
davidy22118 <davidy22118@gmail.com>
2020-04-21 10:09:58 +0300

    apartment-pro

--- apt-filter.js
+++ apt-filter.js
@@ -2,15 +2,18 @@
 
 const must = (radio) => radio.filter(rb => rb.checked).length
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
+const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate,parking) {
     let relevantApts = apartments.filter(a =>
         a.address.toLowerCase().includes(address.toLowerCase() || "") &&
         a.price >= (minPrice || 0) &&
         a.price <= (maxPrice || a.price) &&
         a.rooms >= (minRooms || 0) &&
-        a.rooms <= (maxRooms || a.rooms)
+        a.rooms <= (maxRooms || a.rooms),
+        
+        
     )
-
+    parking = !parking[0].checked
     immediate = !immediate[0].checked
-    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
+    immediate ? relevantApts : relevantApts.filter(a => (a.immediate))
+    return parking ? relevantApts : relevantApts.filter(a => (a.parking))  
 }
